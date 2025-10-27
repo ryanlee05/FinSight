9dcb2f367bd74a9f345ee46bd98ca3856239751e
ryanlee05 <ryanworld2005@icloud.com>
2025-10-27 14:51:50 -0400

    finished api calls, added gif, updated all logic

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Navbar() {
+    return (
+        <div className = "navbar">
+            <h1>FinSight</h1>
+            <h2> - The best Stock overview platform.</h2>
+        </div>
+    );
+}
+export default Navbar;
